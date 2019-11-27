f819f6c739825f7c571b428406d1908f14c1d2ee
Michele Pavanello <michele.pavanello@gmail.com>
2019-11-26 20:44:31 -0500

    done migrating

--- examples/test/__init__.py
+++ examples/test/__init__.py
@@ -4,3 +4,9 @@
 # DFTPY_DATA_PATH='./DATA/'
 # sys.path.append(DFTPY_DATA_PATH)
 # print(sys.path)
+import sys
+import os
+
+SRC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../../src")
+if SRC_DIR not in sys.path:
+    sys.path.insert(0,SRC_DIR)
