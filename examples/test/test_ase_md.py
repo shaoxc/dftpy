8737da9a80420e441c643e4a6a019dbbd210755c
Michele Pavanello <michele.pavanello@gmail.com>
2019-12-28 15:29:21 -0500

    adding md test

---
+++ examples/test/test_ase_md.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import os
+import unittest
+import numpy as np
+from ase.lattice.cubic import FaceCenteredCubic
+from ase.md.langevin import Langevin
+from ase.md.verlet import VelocityVerlet
+from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
+from ase.io.trajectory import Trajectory
+from ase import units
+from ase.io import write, read
+
+from dftpy.config import DefaultOption, OptionFormat
+from dftpy.interface import OptimizeDensityConf
+from dftpy.api.api4ase import DFTpyCalculator
+
+
+
+
+class Test(unittest.TestCase):
+    def test_md(self):
+        dftpy_data_path = os.environ.get('DFTPY_DATA_PATH')
+        conf = DefaultOption()
+        conf['PATH']['pppath'] = os.environ.get('DFTPY_DATA_PATH') 
+        conf['PP']['Al'] = '/Al_lda.oe01.recpot'
+        conf['OPT']['method'] = 'TN'
+        conf['KEDF']['kedf'] = 'WT'
+        conf['JOB']['calctype'] = 'Energy Force'
+        conf['OUTPUT']['time'] = 'False'
+        conf = OptionFormat(conf)
+        calc = DFTpyCalculator(config = conf)
+        atoms = read(filename=dftpy_data_path+'/initial_atoms_md.traj',format='traj',index=-1)
+        atoms.set_calculator(calc)
+        dyn = VelocityVerlet(atoms, 2 * units.fs)
+        dyn.run(3)
+        atoms_fin = read(filename=dftpy_data_path+'/md.traj',index=-1)
+        self.assertTrue( atoms == atoms_fin )
+
+if __name__ == "__main__":
+    unittest.main()
