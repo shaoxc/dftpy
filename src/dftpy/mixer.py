1b1636e5069e645e63c8448fbf936b20cf4bdd67
Shaoxc <shaoxcnmg@gmail.com>
2020-02-05 23:52:08 -0500

    support using dict creat TotalEnergyAndPotential

--- src/dftpy/mixer.py
+++ src/dftpy/mixer.py
@@ -23,25 +23,32 @@ class AbstractMixer(ABC):
     def __call__(self):
         pass
 
-    @abstractmethod
-    def restart(self):
-        pass
-
 
 class LinearMixer(AbstractMixer):
-    def __init__(self, coef = None, orthogonal = False):
+    def __init__(self, coef = None, orthogonal = True, predcond = 'kerker', predcoef = [0.8, 0.1]):
         self.coef = coef
         self.orthogonal= orthogonal
+        self.predcond = predcond
+        self.predcoef = predcoef
 
     def __call__(self, arr1, arr2, coef = [0.5]):
-        if self.orthogonal :
-            a1 = np.cos(coef[0])
-            a2 = np.sin(coef[0])
-        elif len(coef) == 1 :
-            a1 = 0.0
-            a2 = coef[0]
-        else :
-            a1 = coef[0]
-            a2 = coef[1]
-        results = arr1 * a1 + arr2 * a2
+        if self.predcond is None :
+            if self.orthogonal :
+                a1 = np.cos(coef[0])
+                a2 = np.sin(coef[0])
+            elif len(coef) == 1 :
+                a1 = 0.0
+                a2 = coef[0]
+            else :
+                a1 = coef[0]
+                a2 = coef[1]
+            results = arr1 * a1 + arr2 * a2
+        elif self.predcond == 'kerker' :
+            A = self.predcoef[0]
+            q0 = self.predcoef[1] ** 2
+            arr1G = arr1.fft()
+            arr2G = arr2.fft()
+            gg = arr1.grid.get_reciprocal().gg
+            results = arr1G + A * gg/(gg+q0)*arr2G
+            results = results.ifft(force_real=True)
         return results
