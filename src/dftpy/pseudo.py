b5853b175a05ba213e1a3e6898fe15f43d37ea3f
Shaoxc <shaoxcnmg@gmail.com>
2020-05-08 15:12:21 -0400

    little fix

--- src/dftpy/pseudo.py
+++ src/dftpy/pseudo.py
@@ -520,13 +520,15 @@ class ReadPseudo(object):
             with open(Single_PP_file, "r") as outfil:
                 lines = outfil.readlines()
 
+            ibegin = 0
             for i in range(0, len(lines)):
                 line = lines[i]
                 if "END COMMENT" in line:
                     ibegin = i + 3
-                elif line.strip() == "1000" or len(line.strip()) == 1 :
+                elif ibegin > 1 and (line.strip() == "1000" or len(line.strip()) == 1) :
                     iend = i
                     break
+
             line = " ".join([line.strip() for line in lines[ibegin:iend]])
 
             if "1000" in lines[iend] or len(lines[iend].strip()) == 1 :
