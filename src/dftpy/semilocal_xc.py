abdf380313ab086980aa32251dab90581793378b
shaoxc <shaoxcnmg@gmail.com>
2021-02-23 21:13:07 -0500

    move XC class from eDFTpy to here

--- src/dftpy/semilocal_xc.py
+++ src/dftpy/semilocal_xc.py
@@ -5,6 +5,50 @@ from dftpy.field import DirectField
 from dftpy.functional_output import Functional
 from dftpy.time_data import TimeData
 
+class XC :
+    def __init__(self, xc = 'LDA', core_density = None, libxc = True, **kwargs):
+        self.options = {'xc': xc}
+        self.options.update(kwargs)
+        self._core_density = core_density
+        if libxc :
+            self.xcfun = LibXC
+        else :
+            self.xcfun = LDA
+
+    @property
+    def core_density(self):
+        return self._core_density
+
+    @core_density.setter
+    def core_density(self, value):
+        self._core_density = value
+
+    def __call__(self, density, calcType=["E","V"], **kwargs):
+        return self.compute(density, calcType=calcType, **kwargs)
+
+    def compute(self, density, calcType=["E","V"], **kwargs):
+        self.options.update(kwargs)
+        core_density = self.core_density
+        if core_density is None :
+            new_density = density
+        elif density.rank == core_density.rank :
+            new_density = density + core_density
+        elif density.rank == 2 and core_density.rank == 1 :
+            new_density = density.copy()
+            new_density[0] += 0.5 * core_density
+            new_density[1] += 0.5 * core_density
+
+        xc = self.options.get('xc', None)
+        if xc == 'PBE' :
+            xc_kwargs = {"x_str":"gga_x_pbe", "c_str":"gga_c_pbe"}
+        elif xc == 'LDA' :
+            xc_kwargs = {"x_str":"lda_x", "c_str":"lda_c_pz"}
+        else :
+            xc_kwargs = {}
+        self.options.update(xc_kwargs)
+
+        functional = self.xcfun(new_density, calcType = calcType, **self.options)
+        return functional
 
 def CheckLibXC():
     import importlib.util
