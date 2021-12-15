f1ef991518ea0982f099908fa7de0d7c58c3bd54
Kaili Jiang <fudanfuzhong@hotmail.com>
2021-12-15 14:36:35 -0500

    Reduce memory use for linear solver, add property full to hamiltonian

--- src/dftpy/linear_solver.py
+++ src/dftpy/linear_solver.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from dftpy.mpi import MP
+from dftpy.mpi import MP, sprint
 
 __all__ = ['cg', 'bicg', 'bicgstab']
 
@@ -26,22 +26,23 @@ def cg(A, b, x0, tol, maxiter, atol=None, mp=None):
         mp = MP()
 
     atol = _get_atol(tol, atol, _get_norm(b, mp))
-    res = [b - A(x0)]
-    if _get_norm(res[0], mp) < atol:
+    res = b - A(x0)
+    if _get_norm(res, mp) < atol:
         return x0, 0
-    p = [res[0]]
+    p = res
     k = 0
     x = x0
     while k < maxiter:
-        v = A(p[-1])
-        alpha = mp.asum(res[-1] * res[-1]) / mp.asum(p[-1] * v)
-        x = x + alpha * p[-1]
-        res.append(res[-1] - alpha * v)
+        v = A(p)
+        alpha = mp.asum(res * res) / mp.asum(p * v)
+        x = x + alpha * p
+        old_res = res
+        res = old_res - alpha * v
         # sprint(k, mp.amax(np.abs(res[-1])))
-        if _get_norm(res[-1], mp) < atol:
+        if _get_norm(res, mp) < atol:
             return x, 0
-        beta = mp.asum(res[-1] * res[-1]) / mp.asum(res[-2] * res[-2])
-        p.append(res[-1] + beta * p[-1])
+        beta = mp.asum(res * res) / mp.asum(old_res * old_res)
+        p = res + beta * p
         k += 1
     return x, 1
 
@@ -76,36 +77,37 @@ def bicgstab(A, b, x0, tol, maxiter, atol=None, mp=None):
         mp = MP()
 
     atol = _get_atol(tol, atol, _get_norm(b, mp))
-    r = [b - A(x0)]
-    if _get_norm(r[0], mp) < atol:
+    r0 = b - A(x0)
+    if _get_norm(r0, mp) < atol:
         return x0, 0
     rho = [1.0]
     omega = 1.0
     alpha = 1.0
-    v = [0 * b]
-    p = [v[0]]
+    v = 0 * b
+    p = v
     k = 0
     x = x0
-    res = r[0]
+    res = r0
+    r = r0
     while k < maxiter:
-        rho.append(mp.asum(r[0] * r[-1]))
+        rho.append(mp.asum(r0 * r))
         beta = rho[-1] / rho[-2] * alpha / omega
-        p.append(r[-1] + beta * (p[-1] - omega * v[-1]))
-        v.append(A(p[-1]))
-        alpha = rho[-1] / mp.asum(r[0] * v[-1])
-        h = x + alpha * p[-1]
-        res = res - alpha * v[-1]
-        # sprint(k, mp.amax(np.abs(res)))
+        p = r + beta * (p - omega * v)
+        v = A(p)
+        alpha = rho[-1] / mp.asum(r0 * v)
+        h = x + alpha * p
+        res = res - alpha * v
+        # sprint(k, _get_norm(res, mp))
         if _get_norm(res, mp) < atol:
             return h, 0
-        s = r[-1] - alpha * v[-1]
+        s = r - alpha * v
         t = A(s)
         omega = mp.asum(t * s) / mp.asum(t * t)
         x = h + omega * s
         res = res - omega * t
-        # sprint(k, mp.amax(np.abs(res)))
+        # sprint(k, _get_norm(res, mp))
         if _get_norm(res, mp) < atol:
             return x, 0
-        r.append(s - omega * t)
+        r = s - omega * t
         k += 1
     return x, 1
