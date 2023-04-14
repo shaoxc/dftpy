3d5836c22feeb8ad548355d746ebf0e1e915bc9f
shaoxc <shaoxcnmg@gmail.com>
2023-04-13 21:46:25 -0400

    add new xc test

--- examples/test/test_functional_LibXC.py
+++ examples/test/test_functional_LibXC.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 
 from dftpy.functional import Functional
-from dftpy.functional.semilocal_xc import LibXC, PBE
+from dftpy.functional.semilocal_xc import LibXC
 from dftpy.formats import io
 
 
@@ -14,13 +14,9 @@ class Test(unittest.TestCase):
         pytest.importorskip("pylibxc")
         dftpy_data_path = os.environ.get('DFTPY_DATA_PATH')
         rho_r = io.read_density(dftpy_data_path + "/Al_fde_rho.pp")
-        thefuncclass = Functional(type='XC',
-                                  name='LDA',
-                                  libxc=False)
+        thefuncclass = Functional(type='XC', name='LDA', libxc=False)
         func2 = thefuncclass.compute(rho_r)
-        func1 = LibXC(density=rho_r,
-                   x_str='lda_x',
-                   c_str='lda_c_pz')
+        func1 = LibXC(density=rho_r, libxc =['lda_x', 'lda_c_pz'])
         a = func2.energy
         b = func1.energy
         self.assertTrue(np.allclose(a, b))
@@ -29,10 +25,8 @@ class Test(unittest.TestCase):
         pytest.importorskip("pylibxc")
         dftpy_data_path = os.environ.get('DFTPY_DATA_PATH')
         rho_r = io.read_density(dftpy_data_path + "/Al_fde_rho.pp")
-        Functional_LibXC = LibXC(density=rho_r,
-                              x_str='gga_x_pbe',
-                              c_str='gga_c_pbe')
-        Functional_LibXC2 = PBE(rho_r)
+        Functional_LibXC = LibXC(density=rho_r, libxc =['gga_x_pbe', 'gga_c_pbe'])
+        Functional_LibXC2 = Functional(type='XC', xc ='PBE').compute(rho_r)
         self.assertTrue(
             np.isclose(Functional_LibXC2.energy, Functional_LibXC.energy))
 
