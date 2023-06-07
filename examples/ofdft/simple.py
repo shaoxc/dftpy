537a8792043226cdc3105f7724bf2c0a43ce6bf3
shaoxc <shaoxcnmg@gmail.com>
2023-06-07 19:53:46 -0400

    simple example

--- examples/ofdft/simple.py
+++ examples/ofdft/simple.py
@@ -1,22 +1,15 @@
 from dftpy.optimization import Optimization
 from dftpy.functional import Functional, LocalPseudo, TotalFunctional
-from dftpy.formats import io
 from dftpy.grid import DirectGrid
 from dftpy.field import DirectField
-from dftpy.math_utils import ecut2nr
 from dftpy.mpi import MP, sprint
+from dftpy.ions import Ions
+from ase.build import bulk
 
-#
-mp = MP()
-# mp = MP(parallel = True)
-#
-ions = io.read('../DATA/fcc.vasp')
-#
-# PP_list = {'Al': '../DATA/al.lda.recpot'}
-PP_list = ['../DATA/al.lda.recpot']
-#
-nr = ecut2nr(ecut = 20, lattice = ions.cell)
-grid = DirectGrid(lattice=ions.cell, nr=nr, mp=mp)
+mp = MP(parallel = False)
+ions = Ions.from_ase(bulk('Al', 'fcc', a=4.05, cubic=True))
+PP_list = {'Al': '../DATA/al.lda.recpot'}
+grid = DirectGrid(ecut=20, lattice=ions.cell, mp=mp)
 rho_ini = DirectField(grid=grid)
 #
 PSEUDO = LocalPseudo(grid=grid, ions=ions, PP_list=PP_list)
@@ -24,15 +17,12 @@ KE = Functional(type='KEDF', name='WT')
 XC = Functional(type='XC', name='LDA')
 HARTREE = Functional(type='HARTREE')
 #
-rho_ini[:] = ions.get_ncharges() / ions.cell.volume
-#
 funcDict = {'KE' :KE, 'XC' :XC, 'HARTREE' :HARTREE, 'PSEUDO' :PSEUDO}
 EnergyEvaluator = TotalFunctional(**funcDict)
 #
+rho_ini[:] = ions.get_ncharges() / ions.cell.volume
 opt = Optimization(EnergyEvaluator=EnergyEvaluator)
-#
 new_rho = opt.optimize_rho(guess_rho=rho_ini)
 #
-sprint('Calc Energy', comm = mp)
-Enew = EnergyEvaluator.Energy(rho=new_rho, ions=ions)
+Enew = EnergyEvaluator.Energy(rho=new_rho)
 sprint('Energy New (a.u.)', Enew, comm = mp)
