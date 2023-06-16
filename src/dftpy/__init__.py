04bde72d222240eab958060b7521a2b21dbcd82c
shaoxc <shaoxcnmg@gmail.com>
2023-06-16 13:10:44 -0400

    release 2.0.1

--- src/dftpy/__init__.py
+++ src/dftpy/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Pavanello Research Group"
 __contact__ = "m.pavanello@rutgers.edu"
 __license__ = "MIT"
-__version__ = "2.0.1rc0"
+__version__ = "2.0.1"
 __date__ = "2023-06-16"
 
 from .config import *
